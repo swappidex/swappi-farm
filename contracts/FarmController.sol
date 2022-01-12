c87478f8efb16710edcd15f725318f2445181f9a
Fan Wang <fan.wang@confluxnetwork.org>
2022-01-12 18:59:52 +0800

    first commit

---
+++ contracts/FarmController.sol
@@ -0,0 +1,271 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+import "./interfaces/IERC20.sol";
+import "./libraries/SafeERC20.sol";
+
+import "./CakeToken.sol";
+import "./VotingEscrow.sol";
+import "./utils/NeedInitialize.sol";
+import "./roles/WhitelistedRole.sol";
+
+contract FarmController is NeedInitialize, WhitelistedRole {
+    using SafeERC20 for IERC20;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount; // How many tokens the user has provided.
+        uint256 workingSupply; // boosted user share.
+        uint256 rewardPerShare; // Accumulated reward per share.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 token; // Address of token contract.
+        uint256 allocPoint; // How many allocation points assigned to this pool. CAKEs to distribute per block.
+        uint256 lastRewardTime; // Last block number that CAKEs distribution occurs.
+        uint256 totalSupply; // token total supply.
+        uint256 workingSupply; // boosted token supply.
+        uint256 accRewardPerShare; // Accumulated reward per share.
+    }
+
+    CakeToken public cake;
+    VotingEscrow public votingEscrow;
+    // reward tokens created per second.
+    uint256 public rewardPerSecond;
+    // user_boost_share = min(
+    //   user_stake_amount,
+    //   k% * user_stake_amount + (1 - k%) * total_stake_amount * (user_locked_share / total_locked_share)
+    // )
+    uint256 public k;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    // Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint;
+    // The time when reward mining starts.
+    uint256 public startTime;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event UpdateWorkingSupply(
+        address indexed user,
+        uint256 indexed pid,
+        uint256 workingSupply
+    );
+
+    function initialize(
+        address _cake, // reward token
+        uint256 _rewardPerSecond,
+        uint256 _startTime,
+        address _token // first pool
+    ) public onlyInitializeOnce {
+        _addWhitelistAdmin(msg.sender);
+
+        cake = CakeToken(_cake);
+        rewardPerSecond = _rewardPerSecond;
+        startTime = _startTime;
+
+        // first farming pool
+        poolInfo.push(
+            PoolInfo({
+                token: IERC20(_token),
+                allocPoint: 1000,
+                lastRewardTime: startTime,
+                totalSupply: 0,
+                workingSupply: 0,
+                accRewardPerShare: 0
+            })
+        );
+
+        totalAllocPoint = 1000;
+        k = 40;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    function getPoolInfo(uint256 offset)
+        external
+        view
+        returns (PoolInfo[] memory result)
+    {
+        uint256 n =
+            offset + 100 < poolInfo.length ? offset + 100 : poolInfo.length;
+        if (n > offset) {
+            result = new PoolInfo[](n - offset);
+            for (uint256 i = offset; i < n; ++i) {
+                result[i - offset] = poolInfo[i];
+            }
+        }
+    }
+
+    // Add a new lp to the pool. Can only be called by the whitelist admin.
+    function add(
+        uint256 _allocPoint,
+        IERC20 _token,
+        bool _withUpdate
+    ) public onlyWhitelistAdmin {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardTime =
+            block.timestamp > startTime ? block.timestamp : startTime;
+        totalAllocPoint = totalAllocPoint + _allocPoint;
+        poolInfo.push(
+            PoolInfo({
+                token: _token,
+                allocPoint: _allocPoint,
+                lastRewardTime: lastRewardTime,
+                totalSupply: 0,
+                workingSupply: 0,
+                accRewardPerShare: 0
+            })
+        );
+    }
+
+    // Update the given pool's reward allocation point. Can only be called by the owner.
+    function set(
+        uint256 _pid,
+        uint256 _allocPoint,
+        bool _withUpdate
+    ) public onlyWhitelistAdmin {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 prevAllocPoint = poolInfo[_pid].allocPoint;
+        poolInfo[_pid].allocPoint = _allocPoint;
+        if (prevAllocPoint != _allocPoint) {
+            totalAllocPoint = totalAllocPoint - prevAllocPoint + _allocPoint;
+        }
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    function updatePool(uint256 _pid) public {
+        _updatePool(_pid);
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function _updatePool(uint256 _pid) internal {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.timestamp <= pool.lastRewardTime) {
+            return;
+        }
+        if (pool.totalSupply == 0) {
+            pool.lastRewardTime = block.timestamp;
+            return;
+        }
+        uint256 reward =
+            ((block.timestamp - pool.lastRewardTime) *
+                rewardPerSecond *
+                pool.allocPoint) / totalAllocPoint;
+        pool.accRewardPerShare =
+            pool.accRewardPerShare +
+            (reward * 1e18) /
+            pool.workingSupply;
+        pool.lastRewardTime = block.timestamp;
+    }
+
+    function _updateUser(uint256 _pid, address _user)
+        internal
+        returns (uint256 reward)
+    {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        if (user.workingSupply > 0) {
+            reward =
+                (user.workingSupply *
+                    (pool.accRewardPerShare - user.rewardPerShare)) /
+                1e18;
+            if (reward > 0) {
+                cake.mint(_user, reward);
+            }
+            user.rewardPerShare = pool.accRewardPerShare;
+        }
+    }
+
+    function _checkpoint(uint256 _pid, address _user) internal {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        uint256 l = (k * user.amount) / 100;
+        uint256 votingTotal = votingEscrow.totalSupply();
+        if (votingTotal > 0)
+            l +=
+                (((pool.totalSupply * votingEscrow.balanceOf(_user)) /
+                    votingTotal) * (100 - k)) /
+                100;
+        if (l > user.amount) l = user.amount;
+        pool.workingSupply = pool.workingSupply + l - user.workingSupply;
+        user.workingSupply = l;
+        emit UpdateWorkingSupply(_user, _pid, l);
+    }
+
+    // Deposit tokens to Controller for reward allocation.
+    function deposit(uint256 _pid, uint256 _amount)
+        public
+        returns (uint256 reward)
+    {
+        _updatePool(_pid);
+        reward = _updateUser(_pid, msg.sender);
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        if (_amount > 0) {
+            pool.token.safeTransferFrom(
+                address(msg.sender),
+                address(this),
+                _amount
+            );
+            user.amount += _amount;
+            pool.totalSupply += _amount;
+        }
+        _checkpoint(_pid, msg.sender);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw tokens from Controller.
+    function withdraw(uint256 _pid, uint256 _amount)
+        public
+        returns (uint256 reward)
+    {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "FarmController: bad withdraw amount");
+
+        _updatePool(_pid);
+        reward = _updateUser(_pid, msg.sender);
+        if (_amount > 0) {
+            user.amount -= _amount;
+            pool.totalSupply -= _amount;
+            pool.token.safeTransfer(address(msg.sender), _amount);
+        }
+        _checkpoint(_pid, msg.sender);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // kick someone from boosting if his/her locked share expired
+    function kick(uint256 _pid, address _user) public {
+        require(
+            votingEscrow.balanceOf(_user) == 0,
+            "FramController: user locked balance is not zero"
+        );
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        uint256 oldWorkingSupply = user.workingSupply;
+        _updatePool(_pid);
+        _updateUser(_pid, _user);
+        _checkpoint(_pid, _user);
+        require(
+            oldWorkingSupply > user.workingSupply,
+            "FarmController: user working supply is up-to-date"
+        );
+    }
+}
---
+++ contracts/VotingEscrow.sol
@@ -0,0 +1,291 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+import "./interfaces/IERC20.sol";
+import "./libraries/SafeERC20.sol";
+
+import "./utils/ReentrancyGuard.sol";
+import "./utils/NeedInitialize.sol";
+import "./roles/WhitelistedRole.sol";
+
+contract VotingEscrow is ReentrancyGuard, WhitelistedRole, NeedInitialize {
+    using SafeERC20 for IERC20;
+
+    struct LockedBalance {
+        uint256 amount;
+        uint256 unlockTime;
+    }
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    uint256 lastCheckpoint;
+    uint256 totalLocked;
+    uint256 nextWeekSupply;
+
+    IERC20 token;
+
+    uint256 public maxTime; // 4 years
+
+    mapping(address => LockedBalance) public userInfo;
+    mapping(uint256 => uint256) public historySupply;
+    mapping(uint256 => uint256) public unlockSchedule;
+
+    event LockCreated(
+        address indexed account,
+        uint256 amount,
+        uint256 unlockTime
+    );
+
+    event AmountIncreased(address indexed account, uint256 increasedAmount);
+
+    event UnlockTimeIncreased(address indexed account, uint256 newUnlockTime);
+
+    event Withdrawn(address indexed account, uint256 amount);
+
+    function initialize(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        address _token
+    ) public onlyInitializeOnce {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        token = IERC20(token);
+
+        lastCheckpoint = _adjustedTime(block.timestamp);
+
+        maxTime = 4 * 365 * 86400;
+
+        token = IERC20(_token);
+
+        _addWhitelistAdmin(msg.sender);
+    }
+
+    // return start timestamp of lastest week
+    function _adjustedTime(uint256 x) internal pure returns (uint256) {
+        return (x / 1 weeks) * 1 weeks;
+    }
+
+    function createLock(uint256 _amount, uint256 _unlockTime)
+        public
+        nonReentrant
+    {
+        require(
+            msg.sender == tx.origin || isWhitelisted(msg.sender),
+            "VotingEscrow: sender is contract not in whitelist"
+        );
+        require(_amount > 0, "VotingEscrow: amount is zero");
+
+        _unlockTime = _adjustedTime(_unlockTime);
+        LockedBalance storage user = userInfo[msg.sender];
+
+        require(user.amount == 0, "VotingEscrow: withdraw old tokens first");
+        require(
+            _unlockTime > block.timestamp,
+            "VotingEscrow: unlock time < current timestamp"
+        );
+        require(
+            _unlockTime > block.timestamp + maxTime,
+            "VotingEscrow: exceed maxlock time"
+        );
+
+        _checkpoint(_amount, _unlockTime, user.amount, user.unlockTime);
+
+        unlockSchedule[_unlockTime] += _amount;
+        user.unlockTime = _unlockTime;
+        user.amount = _amount;
+
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+
+        emit LockCreated(msg.sender, _amount, _unlockTime);
+    }
+
+    function increaseAmount(address _account, uint256 _amount)
+        external
+        nonReentrant
+    {
+        LockedBalance storage user = userInfo[_account];
+
+        require(_amount > 0, "VotingEscrow: amount is zero");
+        require(user.amount > 0, "VotingEscrow: No existing lock found");
+        require(
+            user.unlockTime > block.timestamp,
+            "VotingEscrow: Cannot add to expired lock"
+        );
+
+        uint256 newAmount = user.amount + _amount;
+        _checkpoint(user.amount, user.unlockTime, newAmount, user.unlockTime);
+        unlockSchedule[user.unlockTime] =
+            unlockSchedule[user.unlockTime] +
+            _amount;
+        user.amount = newAmount;
+
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+
+        emit AmountIncreased(_account, _amount);
+    }
+
+    function increaseUnlockTime(uint256 _unlockTime) external nonReentrant {
+        _unlockTime = _adjustedTime(_unlockTime);
+        LockedBalance storage user = userInfo[msg.sender];
+
+        require(user.amount > 0, "VotingEscrow: No existing lock found");
+        require(
+            user.unlockTime > block.timestamp,
+            "VotingEscrow: Lock expired"
+        );
+        require(
+            _unlockTime > user.unlockTime,
+            "VotingEscrow: Can only increase lock duration"
+        );
+        require(
+            _unlockTime <= block.timestamp + maxTime,
+            "VotingEscrow: Voting lock cannot exceed max lock time"
+        );
+
+        _checkpoint(user.amount, user.unlockTime, user.amount, _unlockTime);
+        unlockSchedule[user.unlockTime] =
+            unlockSchedule[user.unlockTime] -
+            user.amount;
+        unlockSchedule[_unlockTime] = unlockSchedule[_unlockTime] + user.amount;
+        user.unlockTime = _unlockTime;
+
+        emit UnlockTimeIncreased(msg.sender, _unlockTime);
+    }
+
+    function withdraw() external nonReentrant {
+        LockedBalance memory user = userInfo[msg.sender];
+        require(
+            block.timestamp >= user.unlockTime,
+            "VotingEscrow: The lock is not expired"
+        );
+
+        uint256 amount = user.amount;
+        user.unlockTime = 0;
+        user.amount = 0;
+        userInfo[msg.sender] = user;
+
+        token.safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function totalSupply() external view returns (uint256 result) {
+        uint256 w = lastCheckpoint;
+        uint256 currentWeek = _adjustedTime(block.timestamp);
+        uint256 newTotalLocked = totalLocked;
+        uint256 newNextWeekSupply = nextWeekSupply;
+        if (w < currentWeek) {
+            w += 1 weeks;
+            for (; w < currentWeek; w += 1 weeks) {
+                newTotalLocked = newTotalLocked - unlockSchedule[w];
+                newNextWeekSupply =
+                    newNextWeekSupply -
+                    (newTotalLocked * 1 weeks) /
+                    maxTime;
+            }
+            newTotalLocked = newTotalLocked - unlockSchedule[currentWeek];
+            result =
+                newNextWeekSupply -
+                (newTotalLocked * (block.timestamp - currentWeek)) /
+                maxTime;
+        } else {
+            result =
+                newNextWeekSupply +
+                (newTotalLocked * (currentWeek + 1 weeks - block.timestamp)) /
+                maxTime;
+        }
+    }
+
+    function totalSupplyAtTimestamp(uint256 _timestamp)
+        external
+        view
+        returns (uint256)
+    {
+        return _totalSupplyAtTimestamp(_timestamp);
+    }
+
+    function _totalSupplyAtTimestamp(uint256 _timestamp)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 w = _adjustedTime(_timestamp) + 1 weeks;
+        uint256 total = 0;
+        for (; w <= _timestamp + maxTime; w += 1 weeks) {
+            total = total + (unlockSchedule[w] * (w - _timestamp)) / maxTime;
+        }
+        return total;
+    }
+
+    function balanceOf(address _account) external view returns (uint256) {
+        return _balanceOfAtTimestamp(_account, block.timestamp);
+    }
+
+    function balanceOfAtTimestamp(address _account, uint256 _timestamp)
+        external
+        view
+        returns (uint256)
+    {
+        return _balanceOfAtTimestamp(_account, _timestamp);
+    }
+
+    function _balanceOfAtTimestamp(address _account, uint256 _timestamp)
+        private
+        view
+        returns (uint256)
+    {
+        require(
+            _timestamp >= block.timestamp,
+            "VotingEscrow: Must be current or future time"
+        );
+        LockedBalance memory user = userInfo[_account];
+        if (_timestamp > user.unlockTime) {
+            return 0;
+        }
+        return (user.amount * (user.unlockTime - _timestamp)) / maxTime;
+    }
+
+    function _checkpoint(
+        uint256 _newAmount,
+        uint256 _newUnlockTime,
+        uint256 _oldAmount,
+        uint256 _oldUnlockTime
+    ) internal {
+        // update supply to current week
+        uint256 w = lastCheckpoint;
+        uint256 currentWeek = _adjustedTime(block.timestamp);
+        uint256 newTotalLocked = totalLocked;
+        uint256 newNextWeekSupply = nextWeekSupply;
+        if (w < currentWeek) {
+            w += 1 weeks;
+            for (; w <= currentWeek; w += 1 weeks) {
+                historySupply[w] = newNextWeekSupply;
+                newTotalLocked = newTotalLocked - unlockSchedule[w];
+                newNextWeekSupply =
+                    newNextWeekSupply -
+                    (newTotalLocked * 1 weeks) /
+                    maxTime;
+            }
+            lastCheckpoint = currentWeek;
+        }
+
+        // remove old schedule
+        uint256 nextWeek = currentWeek + 1 weeks;
+        if (_oldAmount > 0 && _oldUnlockTime >= nextWeek) {
+            newTotalLocked = newTotalLocked - _oldAmount;
+            newNextWeekSupply =
+                newNextWeekSupply -
+                (_oldAmount * (_oldUnlockTime - nextWeek)) /
+                maxTime;
+        }
+
+        totalLocked = newTotalLocked + _newAmount;
+        nextWeekSupply =
+            newNextWeekSupply +
+            (_newAmount * (_newUnlockTime - nextWeek) + maxTime - 1) /
+            maxTime;
+    }
+}
