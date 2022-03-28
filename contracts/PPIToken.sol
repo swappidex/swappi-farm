0c98c9aa6b6c5d1db7d53aff96819213b2d9d599
Fan Wang <fan.wang@confluxnetwork.org>
2022-03-28 19:01:32 +0800

    chore: rename token

--- contracts/PPIToken.sol
+++ contracts/PPIToken.sol
@@ -5,7 +5,7 @@ import "./erc20/ERC20.sol";
 import "./roles/Ownable.sol";
 
 // PPIToken with Governance.
-contract PPIToken is ERC20("SwappiSwap Token", "PPI", 18), Ownable {
+contract PPIToken is ERC20("Swappi Token", "PPI", 18), Ownable {
     ///  Creates `_amount` token to `_to`. Must only be called by the owner (FarmController).
     function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
